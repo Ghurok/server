4d0099fbc91a601032a882497381041e8f45c29b
Ghurok <ghurok@gmail.com>
2013-03-25 16:29:15 +0100

    [z2303] Add special cases to HandleAuraModIncreaseHealthPercent

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -3502,9 +3502,38 @@ void Aura::HandleAuraModIncreaseEnergyPercent(bool apply, bool /*Real*/)
     GetTarget()->HandleStatModifier(unitMod, TOTAL_PCT, float(m_modifier.m_amount), apply);
 }
 
-void Aura::HandleAuraModIncreaseHealthPercent(bool apply, bool /*Real*/)
+void Aura::HandleAuraModIncreaseHealthPercent(bool apply, bool Real)
 {
-    GetTarget()->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_PCT, float(m_modifier.m_amount), apply);
+    Unit* target = GetTarget();
+
+    // Special case with temporary increase max/current health percentage
+    switch (GetId())
+    {
+        case 23033:                                          // Battle Standard (Alliance)
+        case 23036:                                          // Battle Standard (Horde)
+        {
+            if (Real)
+            {
+                if (apply)
+                {
+                    target->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_PCT, float(m_modifier.m_amount), apply);
+                    target->ModifyHealth((target->GetMaxHealth()/((m_modifier.m_amount/100.0f)+1)) * (m_modifier.m_amount/100.0f));
+                }
+                else
+                {
+                    if (int32(target->GetHealthPercent()) > m_modifier.m_amount)
+                        target->ModifyHealth(-((target->GetMaxHealth()/((m_modifier.m_amount/100.0f)+1)) * (m_modifier.m_amount/100.0f)));
+                    else
+                        target->SetHealth(1);
+                        target->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_PCT, float(m_modifier.m_amount), apply);
+                }
+            }
+            return;
+        }
+    }
+
+    // generic case
+    target->HandleStatModifier(UNIT_MOD_HEALTH, TOTAL_PCT, float(m_modifier.m_amount), apply);
 }
 
 /********************************/
